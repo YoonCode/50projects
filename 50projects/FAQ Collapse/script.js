e71e2f25be9e042a92f261bb5465b8173fb1e314
YoonCode <yooncode>
2022-08-16 00:57:54 +0900

    faq collapse

---
+++ 50projects/FAQ Collapse/script.js
@@ -0,0 +1,8 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+const faqs = document.querySelectorAll('.faq')
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active')
+  })
+})
